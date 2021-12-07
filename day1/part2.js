d75df35ea51888517551ca1c0b5035df4043e272
Ben Waldman <benmw2@gmail.com>
2021-12-07 12:03:11 -0600

    part 2

---
+++ day1/part2.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const readline = require('readline');
+
+async function processLineByLine() {
+  const fileStream = fs.createReadStream('input.txt');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  let inputArray = [];
+
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    inputArray.push(parseInt(line, 10))
+  }
+  return inputArray
+}
+
+async function main() {
+    let increasedCount = 0;
+    inputArray = await processLineByLine();
+    inputArray.forEach((input, i) => {
+        if(i !== 0) {
+            let a = inputArray[i] + inputArray[i+1] + inputArray[i+2] 
+            let b = inputArray[i-1] + inputArray[i] + inputArray[i+1]
+            if( a > b) {
+                increasedCount++;
+            }
+        }
+    });
+    console.log(increasedCount)
+}
+
+main()
