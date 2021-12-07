ab7e971e60f962808c17fbe7fa368554cf826167
Ben Waldman <benmw2@gmail.com>
2021-12-06 20:14:56 -0600

    solution day 1 node

---
+++ day1/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const readline = require('readline');
+
+async function processLineByLine() {
+  const fileStream = fs.createReadStream('input.txt');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  let inputArray = [];
+
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    inputArray.push(parseInt(line, 10))
+  }
+  return inputArray
+}
+
+async function main() {
+    let increasedCount = 0;
+    inputArray = await processLineByLine();
+    inputArray.forEach((input, i) => {
+        if(i !== 0) {
+            if( input > inputArray[i-1]) {
+                increasedCount++;
+            }
+        }
+    });
+    console.log(increasedCount)
+}
+
+main()
